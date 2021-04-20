5a6fca14eb0436900673f0c81c7ea76a09e1f538
muhd ameen <ameens.in19@gamil.com>
2021-04-20 16:22:30 +0530

    back-2-top button added form validation perfect

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ $("#name").blur(function (){
     var text=$(this).val()
     if (text.length<4) {
         $("#err1").show()
+        flag1=1;
+        if(flag1==1){
+            $("#submitbtn").prop("disabled", true);
+        }
        
     } else {
         $("#err1").hide()
-       
-    }
-    var text1=$(this).val()
-    if(/^[a-zA-Z]+$/.test(text1)==false){
-        $("#err2").show()
-       
-    }else{
-        $("#err2").hide()
-       
+        flag1=0;
+        $("#submitbtn").prop("disabled", false);
     }
 })
 
 $("#emailinput").blur(function(){
 
     var inputvalues=$(this).val()
-    if(/^[a-zA-Z0-9-.]+\@[a-zA-Z]+\.[a-zA-Z0-9]+$/.test(inputvalues)==false){
+    if(/^[a-zA-Z0-9-.]+\@[a-zA-Z]+\.[a-zA-Z]+$/.test(inputvalues)==false){
         $("#err2").show()
-       
+        flag2=1;
+        if(flag2==1){
+            $("#submitbtn").prop("disabled", true);
+        }
     }else{
         $("#err2").hide()
-       
+        flag2=0;
+        $("#submitbtn").prop("disabled", false);
+
     }
 })
 
@@ -36,22 +38,48 @@ $("#numberinput").blur(function (){
     var text2=$(this).val()
     if (text2.length<10) {
         $("#err3").show()
-    
+        flag3=1;
+        if(flag3==1){
+            $("#submitbtn").prop("disabled", true);
+        }
     } else {
         $("#err3").hide()
-    
+        flag3=0;
+        $("#submitbtn").prop("disabled", false);
+
     }
 
     var text2=$(this).val()
-    if(/^[0-9+-]+$/.test(text2)==false){
-        $("#err2").show()
-       
+    if(/^\d{10}$/.test(text2)==false){
+        $("#err3").show()
+        flag4=1;
+        if(flag4==1){
+            $("#submitbtn").prop("disabled", true);
+        }
     }else{
-        $("#err2").hide()
-       
+        $("#err3").hide()
+        flag4=0;
+        $("#submitbtn").prop("disabled", false);
+
     }
+
+    
 })
 
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+    $('#toTopBtn').fadeIn();
+    } else {
+    $('#toTopBtn').fadeOut();
+    }
+    });
+    
+    $('#toTopBtn').click(function() {
+    $("html, body").animate({
+    scrollTop: 0
+    }, 1000);
+    return false;
+    });
 
 
 
